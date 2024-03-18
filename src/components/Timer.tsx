4a01124248ca281962b7f5649d86c3b1e61fce83
Dustin <dustindung300@gmail.com>
2024-03-18 19:57:08 +0700

    Implement remaining time in Timer component.

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const interval = useRef<number | null>(null);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+    interval.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
